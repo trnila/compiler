a64146f4025fa3003781f1aacbb72d054ee87ba8
Daniel Trnka <daniel.trnka@gmail.com>
2016-04-15 17:37:30 +0200

    Main class set to analyzer

--- src/lang/Main.java
+++ src/lang/Main.java
@@ -6,18 +6,21 @@ import lang.utils.IRVisitor;
 import lang.utils.NicePrintingVisitor;
 
 import java.io.FileInputStream;
+import java.io.InputStream;
 
 public class Main {
 	public static void main(String[] args) throws Exception {
 		IRVisitor visitor = new NicePrintingVisitor();
 
-		//(new Constant(5, Type.INT)).accept(visitor);
-		LangParser p = new LangParser(new FileInputStream("tests/t3.txt"));
-		//LangParser p = new LangParser(new FileInputStream("x"));
-		//LangParser p = new LangParser(System.in);
+		InputStream is;
+		if(args.length < 1) {
+			is = System.in;
+		} else {
+			is = new FileInputStream(args[0]);
+		}
+
+		LangParser p = new LangParser(is);
 		p.Program().accept(visitor);
-
 		System.out.println(visitor.toString());
-
 	}
 }
